9a6d8daa4189aa08b340ce2f05719c31c8625591
Shaun <shauna.pai@gmail.com>
2026-02-16 05:29:48 +0530

    chore: bump version to 0.1.2

--- src/lib.rs
+++ src/lib.rs
@@ -130,7 +130,7 @@
 
 #![warn(missing_docs)]
 #![allow(clippy::doc_markdown)]
-#![doc(html_root_url = "https://docs.rs/dhan-rs/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/dhan-rs/0.1.2")]
 
 pub mod api;
 pub mod client;
